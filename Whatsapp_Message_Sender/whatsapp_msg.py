b3b1f3dbb50642a68dc3f3cdace59ec8b2fd7044
Ed-Ryan <120978416+Ed-Ryan@users.noreply.github.com>
2023-05-26 11:54:11 -0400

    Create whatsapp_msg.py

---
+++ Whatsapp_Message_Sender/whatsapp_msg.py
@@ -0,0 +1,6 @@
+import pywhatkit
+## The hour and minute are based on a 24 hour time clock
+pywhatkit.sendwhatmsg("<phone number>",
+                      "Message here!",
+                      hour, minute
+                      )
